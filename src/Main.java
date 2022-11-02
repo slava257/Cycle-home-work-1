de4556992ea992a291164559756830ec96b0f62e
79650 <last137954682@gmail.ru>
2022-11-02 21:12:33 +0300

    cycleHW 1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        //Task 1
+        System.out.println("Задание 1");
+        for ( int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+        //Task 2
+        System.out.println("Задание 2");
+        for ( int i=10; i>=1; i--){
+            System.out.println(i);
+        }
+        //Task 3
+        System.out.println("Задание 3");
+        for (int i=0; i<=17;i+=2) {
+            System.out.println(i);
+        }
+        //Task 4
+        System.out.println("Задание 4");
+        for (int i=10; i>=-10;i--){
+            System.out.println(i);
+        }
+    }
+}
