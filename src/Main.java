e76dcf8df00b95a090501732da22d16abe03dede
79650 <last137954682@gmail.ru>
2022-11-02 23:18:00 +0300

    cycleHW 1.2

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ public class Main {
         System.out.println("Задание 9");
         int contribution = 29000;
         int total = 0;
-        for (int i=0;i<12;i++){
+        for (int i=1;i<=12;i++){
             total = total + contribution;
             System.out.println("Месяц "+i+" ,сумма накоплений равна "+total+" рублей");
         }
         //Task 10
         System.out.println("Задание 10");
-        int deposit = 29000;
-        int total1 = 0;
-        for (int i=0;i<12;i++){
+        double depositAtPercent = 29000;
+        double total1 = 0;
+        for (int i=1;i<=12;i++){
+            total1 = total1 + depositAtPercent ;
             total1 =total1 +total1/100;
-            total1 = total1 + deposit ;
             System.out.println("Месяц "+i+" ,сумма накоплений равна "+total1+" рублей");
         }
     }
