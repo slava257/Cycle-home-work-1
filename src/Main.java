cd78ee7cf43d2d52278a8bf54ac1489e83d88807
79650 <last137954682@gmail.ru>
2022-11-02 21:42:27 +0300

    cycleHW 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        //Task 1
-        System.out.println("Задание 1");
-        for ( int i=1; i<=10; i++){
+        //Task 5
+        System.out.println("Задание 6");
+        for (int i=1904; i<=2096;i=i+4){
+            System.out.println(i+" год является високосным");
+        }
+        //Task 6
+        System.out.println("Задание 7");
+        for (int i=7;i<=98;i=i+7){
             System.out.println(i);
         }
-        //Task 2
-        System.out.println("Задание 2");
-        for ( int i=10; i>=1; i--){
-            System.out.println(i);
-        }
-        //Task 3
-        System.out.println("Задание 3");
-        for (int i=0; i<=17;i+=2) {
-            System.out.println(i);
-        }
-        //Task 4
-        System.out.println("Задание 4");
-        for (int i=10; i>=-10;i--){
+        //Task 7
+        System.out.println("Задание 8");
+        for (int i=1;i<=512;i=i*2) {
             System.out.println(i);
         }
     }
